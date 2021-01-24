e012ba710523692919443d4fbc6573f921ceb0ff
rithik <rithikchary21@gmail.com>
2021-01-24 11:31:56 +0530

    fourth  commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyttsx3
 import webbrowser
-import smtplib
+# import smtplib
 import random
 import requests
 import speech_recognition as sr
@@ -96,35 +96,9 @@ if __name__ == '__main__':
  
         elif 'reason for you' in query:
             speak("I was created as a Minor project by Team DUDE ")
- 
-        # elif 'change background' in query:
-        #     ctypes.windll.user32.SystemParametersInfoW(20, 
-        #                                                0, 
-        #                                                "Location of wallpaper",
-        #                                                0)
-
-        elif 'email' in query:
-            speak('Who is the recipient? ')
-            recipient = myCommand()
-
-            if 'I am' in recipient:
-                try:
-                    speak('What should I say? ')
-                    content = myCommand()
-        
-                    server = smtplib.SMTP('smtp.gmail.com', 587)
-                    server.ehlo()
-                    server.starttls()
-                    server.login("Your_Username", 'Your_Password')
-                    server.sendmail('Your_Username', "Recipient_Username", content)
-                    server.close()
-                    speak('Email sent!')
-
-                except:
-                    speak('Sorry ! I am unable to send your message at this moment!')
 
 
-        elif 'nothing' in query or 'abort' in query or 'stop' in query:
+        elif 'nothing more' in query or 'abort' in query or 'stop' in query or 'quit' in query:
             speak('okay')
             speak('Bye ., have a good day.')
             sys.exit()
@@ -137,7 +111,7 @@ if __name__ == '__main__':
             sys.exit()
                                     
         elif 'play music' in query:
-            speak('Sorry!Oonly from local disc i can play music! Say okay! if I should play the music I have!')
+            speak('Sorry! Only from local disc i can play music! Say okay! if I should play the music I have!')
             local = myCommand()
 
             if 'ok' in local:
@@ -177,11 +151,11 @@ if __name__ == '__main__':
                         response = requests.get(url, params=params)
                         dic = response.json()
                         print(dic)
-                    
-                    send_sms("7032972977,8501024681,8074308757,7207775759", myCommand= m)
+                  
+                    send_sms("9515022553",  myCommand= m + '-Asst.DUDE')
 
                 except:
-                    speak('creator was dumb. failed to send your mesaage sorry. enjoy asking anything else to your DUDE.')               
+                    speak('creator sucked up. failed to send your mesaage sorry. enjoy asking anything else to your DUDE.')               
 
         else:
             query = query
@@ -190,8 +164,8 @@ if __name__ == '__main__':
                 try:
                     res = client.query(query)
                     results = next(res.results).text
-                    speak('WOLFRAM-ALPHA says - ')
                     speak('Got it.')
+                    speak('WOLFRAM-ALPHA says - ')
                     speak(results)
                     
                 except:
